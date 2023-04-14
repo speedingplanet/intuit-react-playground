dd073b951452bb14c913549b998a0e7a3903221e
John Paxton <johnpaxton@gmail.com>
2023-04-13 20:04:08 -0400

    End of Day 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import HelloWorld from './HelloWorld';
+
 function App() {
 	return (
 		<div>
 			<h1>React Playground</h1>
+			{/* HelloWorld({userName: 'June'}) ➡️ <div>...</div> */}
+			<HelloWorld userName="June"/>
 		</div>
 	);
 }
---
+++ src/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HelloWorldProps {
+	userName: string;
+}
+
+function HelloWorld({ userName }: HelloWorldProps) {
+	return (
+		<div>Hello, {userName}! The sum of 2 and 5 is {2 + 5}</div>
+	);
+}
+
+export default HelloWorld;
