501f7a88190cb58b060e956a8b1ee28cbb0d114e
John Paxton <johnpaxton@gmail.com>
2023-04-14 17:55:53 -0400

    Remove ESLint problems.

--- src/labs/Lab01.tsx
+++ src/labs/Lab01.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function Lab01() {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	let firstName = 'Christopher';
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	let lastName = 'Smith';
 
 	return (
