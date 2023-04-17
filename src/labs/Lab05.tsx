9cfded1732d98b468355cad216bb762d543a9e8f
John Paxton <johnpaxton@gmail.com>
2023-04-17 17:23:37 -0400

    Update Lab05 to have better typing

--- src/labs/Lab05.tsx
+++ src/labs/Lab05.tsx
@@ -2,7 +2,7 @@ export default function Lab05() {
 	return (
 		<div>
 			<h4>Lab 5</h4>
-			<PagerBar />
+			<PagerBarUsingParameters />
 		</div>
 	);
 }
@@ -27,6 +27,92 @@ export function PagerBar() {
 			<button
 				className="btn btn-info"
 				onClick={handleNextClick}
+			>
+				Next
+			</button>
+		</div>
+	);
+}
+
+// Using React.MouseEventHandler type
+export function BetterPagerBar() {
+	let handleButtonClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+		console.log(`You clicked the ${event.currentTarget.name} button`);
+	};
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	let handleButtonClickEvent = (event: React.MouseEvent<HTMLButtonElement>) => {
+		console.log(`You clicked the ${event.currentTarget.name} button`);
+	};
+
+	return (
+		<div className="button-bar d-flex justify-content-between">
+			<button
+				className="btn btn-info"
+				onClick={handleButtonClick}
+				name="previous"
+			>
+				Previous
+			</button>
+			<button
+				className="btn btn-info"
+				onClick={handleButtonClick}
+				name="next"
+			>
+				Next
+			</button>
+		</div>
+	);
+}
+
+// Invoke the event handler in-line, passing parameters as needed.
+// No event object.
+export function PagerBarUsingParameters() {
+	let handleButtonClick = (label: string) => {
+		console.log(`You clicked the ${label} button`);
+	};
+
+	return (
+		<div className="button-bar d-flex justify-content-between">
+			<button
+				className="btn btn-info"
+				onClick={() => {
+					handleButtonClick('previous');
+				}}
+			>
+				Previous
+			</button>
+			<button
+				className="btn btn-info"
+				onClick={() => {
+					handleButtonClick('next');
+				}}
+			>
+				Next
+			</button>
+		</div>
+	);
+}
+
+// In-line event handler, no need to specify type as it's inferred
+export function InlinePagerBar() {
+	return (
+		<div className="button-bar d-flex justify-content-between">
+			<button
+				className="btn btn-info"
+				onClick={(event) => {
+					console.log(`You clicked the ${event.currentTarget.name} button`);
+				}}
+				name="previous"
+			>
+				Previous
+			</button>
+			<button
+				className="btn btn-info"
+				onClick={(event) => {
+					console.log(`You clicked the ${event.currentTarget.name} button`);
+				}}
+				name="next"
 			>
 				Next
 			</button>
