8bdc402937b978764af8ad47b6e073dec1d1ac61
John Paxton <johnpaxton@gmail.com>
2023-04-17 18:16:51 -0400

    Custom Events

---
+++ src/demos/CustomEvents.tsx
@@ -0,0 +1,41 @@
+export default function EventHandling() {
+	return (
+		<CustomizableButton
+			label="Click me"
+			backgroundColor="blue"
+			textColor="white"
+			// customClick={() => { console.log('Parent event handler'); }}
+			customClick={(event) => {
+				console.dir(event.currentTarget);
+			}}
+		/>
+	);
+}
+
+// Only some valid colors: red, orange, yellow, green, blue, purple
+type CustomColor = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple' | 'black' | 'white';
+type NoBlackOrWhite = Exclude<CustomColor, 'black' | 'white'>;
+
+interface CustomizableButtonProps {
+	label: string;
+	backgroundColor: NoBlackOrWhite;
+	textColor: CustomColor;
+	customClick: React.MouseEventHandler<HTMLButtonElement>;
+	// If you just want a no-op event handler and don't care about the event object
+	customClick2?: () => void;
+}
+export function CustomizableButton(props: CustomizableButtonProps) {
+	let buttonStyle = {
+		backgroundColor: props.backgroundColor,
+		color: props.textColor,
+	};
+	return (
+		<button
+			style={buttonStyle}
+			onClick={props.customClick}
+			className="btn"
+		>
+			{props.label}
+		</button>
+	);
+}
