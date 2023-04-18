335bed3e8b0b85b91b7f825bd507413c3288a308
John Paxton <johnpaxton@gmail.com>
2023-04-18 12:06:38 -0400

    Better staged for testing

--- src/demos/UsingState.tsx
+++ src/demos/UsingState.tsx
@@ -8,38 +8,82 @@ export default function UsingState() {
 	// results[1] is a function that can change results[0];
 	const [color, setColor] = useState('red');
 
-	let handlePickColor: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
-		console.log(`You chose the color ${event.currentTarget.value}`);
-		setColor(event.currentTarget.value);
+	let handlePickColor = (color: string) => {
+		console.log(`You chose the color ${color}`);
+		setColor(color);
 	};
 
 	return (
 		<div className="row">
+			<div className="col">
+				<ColorPicker pickColor={handlePickColor}/>
+			</div>
+			<div className="col">
+				<ColorBox color={color} />
+			</div>
+		</div>
+	);
+}
+
+export function ColorBox({ color }: { color: string }) {
+	return (
+		<div
+			style={{
+				height: '200px',
+				width: '200px',
+				border: '2px black solid',
+				backgroundColor: color,
+			}}
+		></div>
+	);
+}
+
+interface ColorList {
+	value: string;
+	label: string;
+}
+
+interface ColorPickerProps {
+	colorList?: string[] | ColorList[];
+	pickColor: (color: string) => void;
+}
+
+const defaultColors = [
+	'red', 'green', 'blue', 'chartreuse', 'gray', 'black', 'white', 'yellow', 'orange', 'goldenrod',
+];
+export function ColorPicker({ pickColor, colorList }: ColorPickerProps) {
+	if (colorList === undefined || colorList.length === 0) {
+		colorList = defaultColors;
+	}
+
+	return (
+		<>
 			<label htmlFor="color-picker">Pick a color</label>
 			<div className="col">
 				<select
 					name="color-picker"
 					id="color-picker"
 					className="form-select"
-					onChange={handlePickColor}
+					onChange={(event) => {
+						pickColor(event.currentTarget.value);
+					}}
 				>
-					<option value="red">Red</option>
-					<option value="yellow">Yellow</option>
-					<option value="orange">Orange</option>
-					<option value="green">Green</option>
-					<option value="blue">Blue</option>
+					{colorList.map((color, index) => {
+						if (typeof color === 'string') {
+							return <option key={index}>{color}</option>;
+						} else {
+							return (
+								<option
+									key={index}
+									value={color.value}
+								>
+									{color.label}
+								</option>
+							);
+						}
+					})}
 				</select>
 			</div>
-			<div className="col">
-				<div
-					style={{
-						height: '200px',
-						width: '200px',
-						border: '2px black solid',
-						backgroundColor: color,
-					}}
-				></div>
-			</div>
-		</div>
+		</>
 	);
 }
